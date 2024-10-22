27c5ba9fa25085e15756b0947b888b335442e12f
Kauane <86927114+kausdr@users.noreply.github.com>
2024-10-22 09:09:47 -0300

    feat(operation): adds sum

--- app/src/main/java/my/app/calculadorajetpackcompose/MainActivity.kt
+++ app/src/main/java/my/app/calculadorajetpackcompose/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import my.app.calculadorajetpackcompose.ui.theme.CalculadoraJetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,16 +30,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculadoraJetPackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+                CalculadoraUI()
                 }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -47,7 +52,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun LabelTextField(text: String, modifier: Modifier = Modifier) {
+fun TextComposable(text: String, modifier: Modifier = Modifier) {
     Text(text = "$text",modifier = modifier)
 
 }
@@ -57,27 +62,61 @@ fun TextFieldNumber(value: String, onValueChange: (String) -> Unit, label: Strin
     TextField(value, onValueChange,label = {label?.let{  Text(it) }})
 }
 
-@Preview(showBackground = true)
 @Composable
-fun CalculadoraPreview() {
+fun ButtonPlus(number1: String, number2: String, onResultChange: (String) -> Unit) {
+    val num1 = number1.toIntOrNull() ?: 0
+    val num2 = number2.toIntOrNull() ?: 0
+
+   Button(
+    modifier = Modifier
+        .height(50.dp),
+       onClick = {
+           onResultChange((num2+num1).toString())
+       }
+   ) {
+        TextComposable(text = "+")
+   }
+}
+
+
+@Composable
+fun CalculadoraUI() {
     CalculadoraJetPackComposeTheme {
-        var textValue by remember { mutableStateOf("") }
+        var textNumber1 by remember { mutableStateOf("") }
+        var textNumber2 by remember { mutableStateOf("") }
+        var resultado by remember { mutableStateOf("") }
 
         Column (modifier = Modifier
 
         ) {
             Row (modifier = Modifier
             ) {
-                LabelTextField("Número 1:", modifier = Modifier.align(Alignment.CenterVertically))
-                TextFieldNumber(textValue, {textValue = it}, "Número 1:")
+                TextComposable("Número 1:", modifier = Modifier.align(Alignment.CenterVertically))
+                TextFieldNumber(textNumber1, {textNumber1 = it}, "Número 1:")
             }
 
             Row (modifier = Modifier
             ) {
-                LabelTextField("Número 1:", modifier = Modifier.align(Alignment.CenterVertically))
-                TextFieldNumber(textValue, {textValue = it}, "Número 1:")
+                TextComposable("Número 2:", modifier = Modifier.align(Alignment.CenterVertically))
+                TextFieldNumber(textNumber2, {textNumber2 = it}, "Número 2:")
             }
 
+            Row(modifier = Modifier
+            ) {
+                ButtonPlus(textNumber1, textNumber2, onResultChange = { resultado = it })
+            }
+            
+            
+            TextComposable(text = resultado)
+
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculadoraPreview() {
+    CalculadoraJetPackComposeTheme {
+        CalculadoraUI()
     }
 }
